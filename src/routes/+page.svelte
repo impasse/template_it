5e638532fc64601f28a2b10bb1f6de0e910fd703
Yooyi <shyling@live.com>
2025-08-12 23:53:59 +0800

    fix style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,46 +3,53 @@
 
     const liquid = new Liquid();
 
-    let template = $state(localStorage['template'] || '');
-    let jsonObject = $state(JSON.parse(localStorage['jsonObject'] || '{}'));
+    let template = $state(localStorage["template"] || "");
+    let jsonObject = $state(JSON.parse(localStorage["jsonObject"] || "{}"));
 
     $effect(() => {
-        localStorage['template'] = template;
-        localStorage['jsonObject'] = JSON.stringify(jsonObject);
+        localStorage["template"] = template;
+        localStorage["jsonObject"] = JSON.stringify(jsonObject);
     });
 </script>
 
 <svelte:head>
     <title>Liquid Template Renderer</title>
-    <meta name="description" content="A simple Liquid template renderer with Svelte">
+    <meta
+        name="description"
+        content="A simple Liquid template renderer with Svelte"
+    />
 </svelte:head>
 
-<div class="grid grid-cols-2 gap-x-2 p-8 min-h-screen">
-    <div>
+<div class="grid grid-cols-2 grid-rows-2 p-4 w-full h-screen gap-3">
+    <div class="row-span-2 flex flex-col">
         <h1 class="font-bold">liquid template</h1>
-        <textarea class="outline w-full h-full resize-none" bind:value={template}
+        <textarea
+            class="flex-1 outline w-full h-full resize-none p-1"
+            bind:value={template}
         ></textarea>
     </div>
-    <div class="grid grid-cols-1 grid-rows-2 gap-y-8">
-        <div>
-            <h2 class="font-bold">Object</h2>
-            <textarea
-                class="w-full h-full outline resize-none"
-                bind:value={
-                    () => JSON.stringify(jsonObject, null, 4),
-                    (v) => (jsonObject = JSON.parse(v))
-                }
-            ></textarea>
-        </div>
-        <div>
-            <h2 class="font-bold">Rendered</h2>
-            {#await liquid.parseAndRender(template, jsonObject)}
-                <div class="w-full h-full outline">Loading...</div>
-            {:then result}
-                <div class="w-full h-full outline">{result}</div>
-            {:catch e}
-                <div class="w-full h-full outline text-red-500">{e.message}</div>
-            {/await}
-        </div>
+    <div class="flex flex-col">
+        <h2 class="font-bold">Object</h2>
+        <textarea
+            class="flex-1 w-full h-full outline resize-none p-1"
+            bind:value={
+                () => JSON.stringify(jsonObject, null, 4),
+                (v) => (jsonObject = JSON.parse(v))
+            }
+        ></textarea>
+    </div>
+    <div class="flex flex-col">
+        <h2 class="font-bold">Rendered</h2>
+        {#await liquid.parseAndRender(template, jsonObject)}
+            <div class="flex-1 w-full h-full outline p-1">Loading...</div>
+        {:then result}
+            <div class="flex-1 w-full h-full outline overflow-auto p-1">
+                {result}
+            </div>
+        {:catch e}
+            <div class="flex-1 w-full h-full outline text-red-500 p-1">
+                {e.message}
+            </div>
+        {/await}
     </div>
 </div>
